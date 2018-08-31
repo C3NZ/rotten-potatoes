d116340e2a078661b2a236071e927af9d18c5642
C3NZ <epichdd@gmail.com>
2018-08-31 11:18:51 -0700

    Users can now add reviews

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/rotten-potatoes", {useNewUrlParser: true});
 
 const Review = mongoose.model("Review", {
-	title: String
+	title: String,
+	movieTitle: String,
+	review: String
 });
 
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout:"main"}));
 app.set("view engine", "handlebars");
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 //let reviews = [
 //	{title: "Inception"},
@@ -26,6 +31,22 @@ app.get("/", (req, res) => {
 			console.log(err);
 		})
 	});
+
+app.get("/reviews/new", (req, res) => {
+	res.render("reviews-new", {});
+});
+
+app.post("/reviews", (req, res) => {
+	Review.create(req.body)
+		.then(review => {
+			console.log(review)
+			res.redirect("/");
+		})
+		.catch(err => {
+			console.log(err.message)
+		});
+});
+
 
 app.listen(3000, () => {
 	console.log("App listening on port 3000");
