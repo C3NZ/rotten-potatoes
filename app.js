2a2410f500c9b6b2e6e1e95dd0e5babab2041112
C3NZ <epichdd@gmail.com>
2018-09-01 22:36:06 -0700

    Users can now edit reviews after making them

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/rotten-potatoes", {useNewUrlParser: true});
@@ -15,8 +16,9 @@ const Review = mongoose.model("Review", {
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout:"main"}));
 app.set("view engine", "handlebars");
-app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 //let reviews = [
 //	{title: "Inception"},
@@ -30,7 +32,7 @@ app.get("/", (req, res) => {
 		})
 		.catch(err => {
 			console.log(err);
-		})
+		});
 	});
 
 app.get("/reviews/new", (req, res) => {
@@ -50,7 +52,6 @@ app.get("/reviews/:id", (req, res) => {
 app.post("/reviews", (req, res) => {
 	Review.create(req.body)
 		.then(review => {
-			console.log(review)
 			res.redirect(`/reviews/${review._id}`);
 		})
 		.catch(err => {
@@ -58,7 +59,21 @@ app.post("/reviews", (req, res) => {
 		});
 });
 
+app.get("/reviews/:id/edit", (req, res) => {
+	Review.findById(req.params.id, function(err, review){ 
+		res.render("reviews-edit", { review : review });
+	});
+});
 
+app.put("/reviews/:id", (req, res) => {
+	Review.findByIdAndUpdate(req.params.id, req.body)
+		.then(review =>{
+			res.redirect(`/reviews/${review.id}`);
+		})
+		.catch(err => {
+			console.log(err.message);
+		});
+});
 app.listen(3000, () => {
 	console.log("App listening on port 3000");
 });
