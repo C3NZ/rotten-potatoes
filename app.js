0282acf66b770bdc52ef09bc58a971d13f0ac57f
C3NZ <epichdd@gmail.com>
2018-09-03 10:59:36 -0700

    Added the  env variable to be an option for the app to listen on

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/reviews', reviews);
 app.use('/reviews/comments', comments);
 
 //Server start
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log("App listening on port 3000");
 });
 
