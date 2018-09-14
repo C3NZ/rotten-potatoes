55fd0711c9cb7c49fec791110a9360d5c725cadc
C3NZ <cenz@cenz.io>
2018-09-13 23:18:09 -0700

    Added support for moviedb api and now proceding to make the UI

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+//Movie db access
+const movieDb = require("moviedb-promise");
+const moviedb = new movieDb("c45bddf90209a92e0e385fcae855b53c")
+
+
 router.get('/', (req, res) => {
-	res.render('movies-index');
+	moviedb.miscNowPlayingMovies().then(response => {
+		console.log(response.results)
+		res.render("movies-index", {movies: response.results});
+	});
 });
 
 module.exports = router;
