172a2c636eab61c6d61042ffee9090f35211384d
C3NZ <cenz@cenz.io>
2018-09-16 17:40:31 -0700

    Movies are now listed by genre

--- controllers/movies.js
+++ controllers/movies.js
@@ -7,9 +7,26 @@ const moviedb = new movieDb("c45bddf90209a92e0e385fcae855b53c")
 
 
 router.get('/', (req, res) => {
-	moviedb.miscNowPlayingMovies().then(response => {
-		console.log(response.results)
-		res.render("movies-index", {movies: response.results});
+	moviedb.miscNowPlayingMovies().then(movies => {
+		moviedb.genreMovieList().then(genres => {
+				res.render("movies-index", { 
+					movies: movies.results,
+					genres: genres.genres,
+					helpers: {
+						compare: function(genreId, movieGenres, options) {
+							console.log(`Genre Id: ${genreId}`);
+							console.log(`movieGenres ${movieGenres}`);
+							if (movieGenres.includes(genreId)){
+								return options.fn(this);
+							}else{
+								return options.inverse(this);
+							}
+						}
+					}
+				});
+			//	console.log(movies.results)
+			//	console.log(genres.genres)
+		});
 	});
 });
 
