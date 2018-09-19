239c92af74bd6b6c8215196d78e90b48a8ca5940
C3NZ <cenz@cenz.io>
2018-09-18 18:56:05 -0700

    Added a new route for showing a single movie

--- controllers/movies.js
+++ controllers/movies.js
@@ -9,24 +9,37 @@ const moviedb = new movieDb("c45bddf90209a92e0e385fcae855b53c")
 router.get('/', (req, res) => {
 	moviedb.miscNowPlayingMovies().then(movies => {
 		moviedb.genreMovieList().then(genres => {
-				res.render("movies-index", { 
-					movies: movies.results,
-					genres: genres.genres,
-					helpers: {
-						compare: function(genreId, movieGenres, options) {
-							console.log(`Genre Id: ${genreId}`);
-							console.log(`movieGenres ${movieGenres}`);
-							if (movieGenres.includes(genreId)){
-								return options.fn(this);
-							}else{
-								return options.inverse(this);
-							}
+			res.render("movies-index", { 
+				movies: movies.results,
+				genres: genres.genres,
+				helpers: {
+					compare: function(genreId, movieGenres, options) {
+						if (movieGenres.includes(genreId)){
+							return options.fn(this);
+						}else{
+							return options.inverse(this);
 						}
 					}
-				});
-			//	console.log(movies.results)
-			//	console.log(genres.genres)
+				}
+			});
 		});
+	});
+});
+
+router.get('/movies/:id', (req, res) => {
+	moviedb.movieInfo({id: req.params.id}).then(movie => {
+		if(movie.video){
+			moviedb.movieVideos({id: req.params.id}).then(videos => {
+				movie.trailer_youtube_id = videos.results[0].key
+				renderTemplate(movie);
+			});
+		}else{
+			renderTemplate(movie);
+		}
+		function renderTemplate(movie){
+			res.render('movies-show', {movie: movie})
+		}
+	
 	});
 });
 
